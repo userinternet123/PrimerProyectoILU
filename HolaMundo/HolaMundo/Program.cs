83ebbba9b32ae0a007dce94c3740b6382c3b3be2
rus <rus@launion.com.gt>
2019-07-31 08:32:41 -0600

    Cambio en mensaje

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -10,7 +10,7 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Rubén");
             Console.ReadKey();
         }
     }
